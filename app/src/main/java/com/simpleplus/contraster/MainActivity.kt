85b70021460e9c980b083fb5f70eddbfb421303d
Bruno Baccho Terra <brunobacchot@hotmail.com>
2021-06-01 17:20:29 -0300

    Improvements to text displays.

--- app/src/main/java/com/simpleplus/contraster/MainActivity.kt
+++ app/src/main/java/com/simpleplus/contraster/MainActivity.kt
@@ -2,7 +2,10 @@ package com.simpleplus.contraster
 
 import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
 import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.ColorUtils
@@ -26,11 +29,14 @@ class MainActivity : AppCompatActivity(), PickersUtil.OnPickerChangeListener {
     //Utils
     private lateinit var pickersUtil: PickersUtil
 
+    //String components
+    private lateinit var spannableString: SpannableString
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binder.root)
-        pickersUtil = PickersUtil(binder, this,this)
-        binder.activityMainTargetTxt3.text = calculateContrastRatio(pickersUtil.selectedBackgroundColor,pickersUtil.selectedForegroundColor)
+        pickersUtil = PickersUtil(binder, this, this)
+        calculateContrastRatio(pickersUtil.selectedBackgroundColor,pickersUtil.selectedForegroundColor)
 
     }
 
@@ -39,33 +45,36 @@ class MainActivity : AppCompatActivity(), PickersUtil.OnPickerChangeListener {
         when (binder.activityMainMaterialBtnGroup.checkedButtonId) {
             binder.activityMainBtnBackground.id -> binder.root.setBackgroundColor(color)
             else -> {
-                binder.activityMainTargetTxt1.setTextColor(color)
-                binder.activityMainTargetTxt2.setTextColor(color)
-                binder.activityMainTargetTxt3.setTextColor(color)
+                binder.activityMainTxtAppName.setTextColor(color)
+                binder.activityMainTxtContrastRatio.setTextColor(color)
+                binder.activityMainTxtMessage.setTextColor(color)
+                binder.activityMainTxtInformation.setTextColor(color)
             }
         }
 
     }
 
-    private fun calculateContrastRatio(backgroundColor: Int, foregroundColor: Int) : String{
+    private fun calculateContrastRatio(backgroundColor: Int, foregroundColor: Int) {
 
-        val ratio = ColorUtils.calculateContrast(foregroundColor,backgroundColor)
+        val ratio = ColorUtils.calculateContrast(foregroundColor, backgroundColor)
 
+        spannableString = when (ratio) {
 
-        return when(ratio) {
-
-            in 7.0..21.0 -> "$ratio AAA"
-            in 4.5..6.9 -> "$ratio AA"
-            in 3.0..4.49 -> "$ratio AA+"
-            else -> "$ratio FAIL"
+            in 7.0..21.0 -> SpannableString("Aa ${String.format("%.2f",ratio)} AAA")
+            in 4.5..6.9 -> SpannableString("Aa ${String.format("%.2f",ratio)} AA")
+            in 3.0..4.49 -> SpannableString("Aa ${String.format("%.2f",ratio)} AA+")
+            else -> SpannableString("Aa ${String.format("%.2f",ratio)} FAIL")
 
         }
+
+        spannableString.setSpan(RelativeSizeSpan(3f),0,3,Spanned.SPAN_INCLUSIVE_EXCLUSIVE)
+        binder.activityMainTxtContrastRatio.text = spannableString
 
     }
 
     override fun onColorChanged(currentColorInt: Int, backgroundColor: Int, foregroundColor: Int) {
         paintSelectedLayout(currentColorInt)
-        binder.activityMainTargetTxt3.text = calculateContrastRatio(backgroundColor,foregroundColor)
+        calculateContrastRatio(backgroundColor,foregroundColor)
 
     }
 
